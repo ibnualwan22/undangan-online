e77cc308a5bf2eb024137def66e3b51e37b5d229
ibnualwan22 <allwanecahzobo@gmail.com>
2025-09-09 05:37:34 +0700

    Siapkan konfigurasi untuk Vercel

--- config/database.js
+++ config/database.js
@@ -1,25 +1,20 @@
-// Menggunakan dotenv untuk memuat variabel dari file .env
 require('dotenv').config();
 
-// Objek konfigurasi untuk lingkungan yang berbeda
 const config = {
-  development: {
+  development: { // Untuk lokal
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
   },
-  production: {
-    // Variabel untuk produksi akan diambil dari environment variables di server hosting
+  production: { // Untuk Vercel
     host: process.env.PROD_DB_HOST,
     user: process.env.PROD_DB_USER,
     password: process.env.PROD_DB_PASSWORD,
-    database: process.env.PROD_DB_NAME
+    database: process.env.PROD_DB_NAME,
+    ssl: { "rejectUnauthorized": true } // PlanetScale memerlukan SSL
   }
 };
 
-// Menentukan environment mana yang sedang berjalan, defaultnya adalah 'development'
 const env = process.env.NODE_ENV || 'development';
-
-// Ekspor konfigurasi yang sesuai dengan environment saat ini
 module.exports = config[env];
